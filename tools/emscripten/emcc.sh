66d778adbaa7ad516ba989be05baf4cd4619a280
Attila Oláh <atl@google.com>
2021-01-28 13:54:48 +0000

    Use readlink -f … instead of ${PWD}/… to get the absolute path.

--- tools/emscripten/emcc.sh
+++ tools/emscripten/emcc.sh
@@ -4,10 +4,10 @@
 # Set the EM_CONFIG environment variable to an absolute path.
 # This avoids a bug where subcommands fail to find the config.
 
-export EM_CONFIG="${PWD}/${EM_CONFIG}"
-export EM_BINARYEN_ROOT="${PWD}/${EM_BINARYEN_ROOT}"
-export EM_LLVM_ROOT="${PWD}/${EM_LLVM_ROOT}"
-export EM_NODE_JS="${PWD}/${EM_NODE_JS}"
+export EM_CONFIG="$(readlink -f "${EM_CONFIG}")"
+export EM_BINARYEN_ROOT="$(readlink -f "${EM_BINARYEN_ROOT}")"
+export EM_LLVM_ROOT="$(readlink -f "${EM_LLVM_ROOT}")"
+export EM_NODE_JS="$(readlink -f "${EM_NODE_JS}")"
 
 # Same applies for the Python interpreter.
 export PYTHON="$(readlink -f "${PYTHON}")"
