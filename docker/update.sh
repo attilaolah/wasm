bb739d2b34b9e6ca248ed4c79a671ae869006647
Attila Oláh <atl@google.com>
2021-01-28 16:15:41 +0000

    No need to install Python in the Docker container any more.

--- docker/update.sh
+++ docker/update.sh
@@ -8,14 +8,13 @@ apt-get --yes dist-upgrade 2>&1
 # Install Bazel, and:
 # - Git, for git_archive() repository rules.
 # - Make and CMake, for rules_foreign_cc rules.
-# - Python 3, as Emscripten requires Python 3.5+.
 # - GNU M4, required by //lib/gmp. (Maybe add this to //tools some day.)
 # https://docs.bazel.build/versions/master/install-ubuntu.html#install-on-ubuntu
 apt-get --yes install curl gnupg 2>&1
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/bazel.gpg
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" >> /etc/apt/sources.list.d/bazel.list
 apt-get update
-apt-get --yes install bazel git make cmake python3 m4 2>&1
+apt-get --yes install bazel git make cmake m4 2>&1
 
 # Self-destruct.
 rm /etc/update.sh
