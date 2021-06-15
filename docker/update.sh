fce5442bb08e469b496bceaba29ffe5f6d0d3287
Attila Oláh <atl@google.com>
2021-06-15 15:56:59 +0000

    Add libfindbin-libs-perl, required by //lib/openssl.

--- docker/update.sh
+++ docker/update.sh
@@ -5,19 +5,24 @@
 apt-get update
 apt-get --yes dist-upgrade 2>&1
 
-# Install Bazel, and:
-# - GNU M4, required by //lib/gmp. (Maybe add this to //tools some day.)
+# Install Bazel:
 # https://docs.bazel.build/versions/master/install-ubuntu.html#install-on-ubuntu
 apt-get --yes install curl gnupg 2>&1
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/bazel.gpg
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" >> /etc/apt/sources.list.d/bazel.list
 apt-get update
-apt-get --yes install bazel m4 2>&1
+apt-get --yes install bazel 2>&1
 
-# TODO: Required by //lib/cblas, to be added to the build system.
-apt-get --yes install gfortran
+# Required by //lib/gmp:
+apt-get --yes install m4 2>&1
 
-# TODO: Required by //lib/grass, to be added to the build system.
+# Required by //lib/openssl:
+apt-get --yes install libfindbin-libs-perl 2>&1
+
+# Required by //lib/cblas:
+apt-get --yes install gfortran 2>&1
+
+# Required by //lib/grass:
 apt-get --yes install python3-distutils python3-six bison flex gettext 2>&1
 
 # Self-destruct.
