b350d1b0a34e8f86901eb39a9b517011a4a0a2d8
Attila Oláh <atl@google.com>
2021-09-20 13:37:28 +0000

    Update Docker image to Ubuntu 21.04.

--- docker/update.sh
+++ docker/update.sh
@@ -1,13 +1,16 @@
 # To be called from the Docker container.
 # DO NOT RUN outside of the Docker container.
 
+# Prevent Apt from asking too many questions.
+export DEBIAN_FRONTEND=noninteractive
+
 # First do a system upgrade.
 apt-get update
 apt-get --yes dist-upgrade 2>&1
 
 # Install Bazel:
 # https://docs.bazel.build/versions/master/install-ubuntu.html#install-on-ubuntu
-apt-get --yes install curl gnupg 2>&1
+apt-get --yes install apt-transport-https curl gnupg 2>&1
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/bazel.gpg
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" >> /etc/apt/sources.list.d/bazel.list
 apt-get update
