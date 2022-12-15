1f58e25d4504c85bceabe3d020b0ead6c88cd59f
Attila Oláh <atl@google.com>
2022-12-15 18:11:30 +0000

    Return a js_sys::Error from the main.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
-use wasm_bindgen::prelude::*;
+use js_sys::Error;
+use wasm_bindgen::prelude::wasm_bindgen;
 
 mod layout;
 mod notebook_config;
 
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
 #[wasm_bindgen(start)]
-pub async fn main() -> Result<(), JsValue> {
-    let win = web_sys::window().expect("`window` not found");
-    let doc = win.document().expect("`window.document` not found");
-    let body = doc.body().expect("`window.document.body` not found");
+pub async fn main() -> Result<(), Error> {
+    let win = web_sys::window().ok_or(Error::new("`window` not found"))?;
+    let doc = win.document().ok_or(Error::new("`document` not found"))?;
+    let body = doc.body().ok_or(Error::new("`body` not found"))?;
 
     layout::load_external()?;
     layout::display_content(&doc, &body).await?;
