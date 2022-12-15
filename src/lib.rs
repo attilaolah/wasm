7ed3611ae285e993e310dac2e7e53081ed84efcb
Attila Oláh <atl@google.com>
2022-12-15 00:15:29 +0000

    Add DOM nodes from Rust.

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,23 @@
 use wasm_bindgen::prelude::*;
-use web_sys::console;
 
-
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
-// This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
-pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    //#[cfg(debug_assertions)]
-    //console_error_panic_hook::set_once();
+pub fn run() -> Result<(), JsValue> {
+    let document = web_sys::window()
+      .expect("`window` not found")
+      .document().expect("`.document` not found");
 
+    // Manufacture the element:
+    let p = document.create_element("p")?;
+    p.set_text_content(Some("Hello, Rust!"));
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    // Append to the body:
+    document
+      .body().expect("`.body` not found")
+      .append_child(&p)?;
 
     Ok(())
 }
