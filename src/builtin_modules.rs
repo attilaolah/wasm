60ee4efa039377f64b330aba613ee842e61991ce
Attila Oláh <atl@google.com>
2022-12-27 12:54:49 +0000

    Register built-in CSS module.

--- src/builtin_modules.rs
+++ src/builtin_modules.rs
@@ -1,7 +1,8 @@
 use js_sys::Error;
-use web_sys::{console, HtmlDivElement};
+use web_sys::{console, HtmlDivElement, HtmlStyleElement};
 
 use crate::code_blocks::{get_out, get_src, get_text, set_res};
+use crate::dom_helpers::{document, create_element};
 
 pub fn mod_js(_cell: &HtmlDivElement) -> Result<(), Error> {
     console::log_1(&"todo: run js".into());
@@ -10,11 +11,9 @@ pub fn mod_js(_cell: &HtmlDivElement) -> Result<(), Error> {
 }
 
 pub fn mod_html(cell: &HtmlDivElement) -> Result<(), Error> {
-    let code_src = get_src(&cell)?;
-    let raw_html = get_text(&code_src)?;
     let out = get_out(&cell)?;
-    out.set_inner_html(&raw_html);
 
+    out.set_inner_html(&get_text(&get_src(&cell)?)?);
     if let Some(result) = out.last_element_child() {
         set_res(&cell, &result)?;
     }
@@ -22,8 +21,24 @@ pub fn mod_html(cell: &HtmlDivElement) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn mod_css(_cell: &HtmlDivElement) -> Result<(), Error> {
-    console::log_1(&"todo: run css".into());
+pub fn mod_css(cell: &HtmlDivElement) -> Result<(), Error> {
+    let out = get_out(&cell)?;
+    let style: HtmlStyleElement = create_element("style")?;
+
+    style.set_inner_html(&get_text(&get_src(&cell)?)?);
+    out.set_inner_html(&style.outer_html());
+
+    // TODO: Ideally we'd use an iterator, but that requires
+    // https://github.com/rustwasm/wasm-bindgen/issues/1036 to be implemented.
+    let style_sheets = document()?.style_sheets();
+    for i in 0..style_sheets.length() {
+        if let Some(style_sheet) = style_sheets.get(i) {
+            if style.is_same_node(style_sheet.owner_node().as_ref()) {
+                set_res(&cell, &style_sheet)?;
+                break;
+            }
+        }
+    }
 
     Ok(())
 }
