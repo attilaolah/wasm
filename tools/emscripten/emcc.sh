585107fe814c0103e3d300eb45af03a979839331
Attila Oláh <attilaolah@gmail.com>
2023-10-29 11:18:17 +0100

    Fix //lib/ffmpeg wasm32 build.
    There are still issues with the assembly code.

--- tools/emscripten/emcc.sh
+++ tools/emscripten/emcc.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-export ROOT_DIR="${PWD}"
+# Resolve a symbolic link.
+# TODO: Check whether this is still necessary.
 export EM_CONFIG="$(readlink -f "${EM_CONFIG}")"
 
 # The actual location of the 'emcc' binary also comes from the environment.
