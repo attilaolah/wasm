eaadc7fe5c6c42f7bd7a0e9d34daf1eb436aa4fe
Attila Oláh <attila@dorn.haus>
2026-03-24 11:06:14 +0100

    chore: remove archived dist and examples trees

--- dist/LICENSE.js
+++
@@ -1,8 +0,0 @@
-/**
- * WebNB: Interactive Markdown notebooks
- *
- * @license MIT <https://opensource.org/licenses/MIT>
- * @author Attila Oláh <https://attilaolah.eu>
- * @namespace
- * @public
- */
